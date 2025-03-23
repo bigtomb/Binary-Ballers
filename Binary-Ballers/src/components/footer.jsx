39b9fa710f0713fbea15edec7e2975c2ba07d917
Hayes Chiasson <hayeschiasson1@yahoo.com>
2025-03-23 10:56:11 -0500

    Home page finished, colors edited

--- Binary-Ballers/src/components/footer.jsx
+++ Binary-Ballers/src/components/footer.jsx
@@ -4,27 +4,26 @@ function Footer() {
 
     return (
       <>
-        <footer class="bg-caramel p-2">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span class=" text-md text-black font-primary rounded hover:text-lght-green active:text-money-green sm:text-center ">© 2025 Team Binary Bandits All Rights Reserved.
+        <footer className="bg-caramel p-2">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span className=" text-md text-black font-primary rounded hover:text-lght-green active:text-money-green sm:text-center ">© 2025 Team Binary Bandits All Rights Reserved.
             </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="#" class="text-md text-black font-primary rounded hover:text-lght-green active:text-money-green me-4 md:me-6">Home</a>
+                    <a href="#" className="text-md text-black font-primary rounded hover:text-lght-green active:text-money-green me-4 md:me-6">Home</a>
                 </li>
                 <li>
-                    <a href="#" class="text-md text-black font-primary rounded hover:text-lght-green active:text-money-greenme-4 md:me-6">Game</a>
+                    <a href="#" className="text-md text-black font-primary rounded hover:text-lght-green active:text-money-greenme-4 md:me-6">Game</a>
                 </li>
                 <li>
-                    <a href="#" class="text-md text-black font-primary rounded hover:text-lght-green active:text-money-green me-4 md:me-6">Resources</a>
+                    <a href="#" className="text-md text-black font-primary rounded hover:text-lght-green active:text-money-green me-4 md:me-6">Resources</a>
                 </li>
                 <li>
-                    <a href="#" class="text-mdtext-black font-primary rounded hover:text-lght-green active:text-money-green">About</a>
+                    <a href="#" className="text-md text-black font-primary rounded hover:text-lght-green active:text-money-green">About</a>
                 </li>
             </ul>
             </div>
         </footer>
-
       </>
     )
   }
--- Binary-Ballers/src/components/navbar.jsx
+++ Binary-Ballers/src/components/navbar.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../assets/images/life-ledger-logo.png'
 
 function Navbar() {
 
     return (
       <>
-        <nav className="bg-drk-gray fixed w-full z-20 top-0 nav">
+        <nav className="bg-davy-gray fixed w-full z-20 top-0 nav">
             <div className=" flex flex-wrap items-center justify-between mx-4 p-5">
                 <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                {/* <img className="h-8" src={} alt="Logo" /> */}
+                <img className="h-10 mt-0 mb-0 hover:" src={Logo} alt="Logo" /> {/*Logo imag looks best at h-15-16*/}
                 </Link>
-                <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"> {/*Below: ternary operator to display login button or user icon based on user login/ authorization*/}
+                <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse ">
                 </div>
-                <ul className="bg-drk-gray flex flex-col font-medium p-4 md:p-0 mt-4  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:lght-green"> {/*Border box for main links(both in main and mini menu*/}
+                <ul className="bg-davy-gray flex flex-col font-medium p-4 md:p-0 mt-4  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:lght-green"> {/*Border box for main links(both in main and mini menu*/}
                     <li>
-                    <Link to="/" className="block py-2 px-3 text-lg text-caramel font-primary rounded hover:text-lght-green active:text-money-green md:p-0 ">
+                    <Link to="/" className="block py-2 px-3 text-lg text-bank-green font-primary rounded hover:text-lght-green active:text-money-green md:p-0 ">
                         Home
                     </Link>
                     </li>
                     <li>
-                    <Link to="/game" className="block py-2 px-3 text-lg text-caramel font-primary rounded hover:text-lght-green active:text-money-green md:p-0 ">
+                    <Link to="/game" className="block py-2 px-3 text-lg text-bank-green font-primary rounded hover:text-lght-green active:text-money-green md:p-0 ">
                         Game
                     </Link>
                     </li>
                     <li>
-                    <Link to="/resources" className="block py-2 px-3 text-lg text-caramel font-primary  rounded hover:text-lght-green active:text-money-green md:p-0 ">
+                    <Link to="/resources" className="block py-2 px-3 text-lg text-bank-green font-primary  rounded hover:text-lght-green active:text-money-green md:p-0 ">
                         Resources
                     </Link>
                     </li>
